52b27234a318dcc5b6f747e13cce2d1a7ac89f06
Paul AMAR <aos.paul@gmail.com>
2013-12-10 11:53:27 +0100

    Add better example in the APIExample to iterate on all results

--- APIExample.py
+++ APIExample.py
@@ -3,10 +3,17 @@ from eztvAPI import *
 test_api = eztvAPI().TV_Show('Game Of Thrones')
 
 # get all the seasons from Game Of Thrones
-print test_api.seasons()
+seasons = test_api.seasons()
+for season in seasons:
+	for episode in seasons[season]:
+		# will print the magnet link of all episodes, in all seasons
+		print seasons[season][episode]
 
 # specific season
-print test_api.season(3)
+episodes = test_api.season(3)
+for episode in episodes:
+	# will print the magnet link for all episodes
+	print episodes[episode]
 
 # specific episode
 print test_api.episode(3, 10)
